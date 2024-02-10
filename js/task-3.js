e2f470ab6fb975e615b992fb38df5be0d7640c97
ynazaruk <nazaruk.ya@gmail.com>
2024-02-11 00:31:59 +0200

    1-2

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    for (const key in profile) {
+      profile.username = newName;
+    }
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
